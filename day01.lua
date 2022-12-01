4e4483c42ec1af897fb4d72d7189b753b988280d
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-12-01 09:08:52 +0100

    Solve day 1

---
+++ day01.lua
@@ -0,0 +1,57 @@
+result_a = 24000
+result_b = 45000
+
+function build_calories(filename)
+	io.input(filename)
+	calories = {}
+	elf_counter = 1
+
+	for line in io.lines() do
+		if calories[elf_counter] == nil then
+			calories[elf_counter] = 0
+		end
+		if line ~= "" then
+			cal = tonumber(line)
+			calories[elf_counter] = calories[elf_counter] + cal
+		else
+			elf_counter = elf_counter + 1
+		end
+	end
+
+	return calories
+end
+
+function run_a(filename)
+	calories = build_calories(filename)
+	return math.max(table.unpack(calories))
+end
+
+function test_a()
+	assert(run_a("examples/day01.txt") == result_a)
+end
+
+test_a()
+print(run_a("puzzles/day01.txt"))
+
+function run_b(filename)
+	calories = build_calories(filename)
+	sum = 0
+	for i = 1, 3 do
+		max = math.max(table.unpack(calories))
+		for i, val in ipairs(calories) do
+			if val == max then
+				table.remove(calories, i)
+				break
+			end
+		end
+		sum = sum + max
+	end
+	return sum
+end
+
+function test_b()
+	assert(run_b("examples/day01.txt") == result_b)
+end
+
+test_b()
+print(run_b("puzzles/day01.txt"))

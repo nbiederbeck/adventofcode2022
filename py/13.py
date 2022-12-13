abcc5722cfdd68cd65aa51b5b780777536eda303
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-12-13 08:35:08 +0100

    day 13

--- py/13.py
+++ py/13.py
@@ -1,3 +1,6 @@
+from functools import cmp_to_key
+
+
 def parse(f):
     return [list(map(eval, d.split("\n"))) for d in open(f).read()[:-1].split("\n\n")]
 
@@ -50,8 +53,31 @@ def part_a(input):
     return sum(indices_right_order)
 
 
+def wrap_check(*tup):
+    c = check(*tup)
+    if c is True:
+        return -1
+    if c is False:
+        return 1
+    return 0
+
+
+def part_b(input):
+    div_a = [[2]]
+    div_b = [[6]]
+    lists = [div_a, div_b]
+    for left, right in input:
+        lists.append(left)
+        lists.append(right)
+
+    sort = sorted(lists, key=cmp_to_key(wrap_check))
+    return (sort.index(div_a) + 1) * (sort.index(div_b) + 1)
+
+
 if __name__ == "__main__":
     ex = parse("examples/13.txt")
-    assert part_a(ex) == 13
     f = parse("build/13")
+    assert part_a(ex) == 13
     print(part_a(f))
+    assert part_b(ex) == 140
+    print(part_b(f))

483d026f27727af11b45f91ba650af07c9c6f20c
Noah Biederbeck <noah.biederbeck@tu-dortmund.de>
2022-12-14 19:24:15 +0100

    Add a little structure

--- aoc.py
+++ aoc.py
@@ -2,7 +2,7 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("days", nargs="+")
+parser.add_argument("days", nargs="+", type=int)
 args = parser.parse_args()
 
 from pathlib import Path
@@ -13,7 +13,9 @@ def main(args):
     for day in args.days:
         file = Path(f"py/{day}.py")
         if file.exists():
+            print(f"Advent of Code, Day {day:02d}")
             run(["python", file])
+            print(f"----------------------")
 
 
 if __name__ == "__main__":
